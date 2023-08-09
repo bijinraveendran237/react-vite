926550a41c20f59d1c3935b87deb6163e37dd52c
bijinraveendran237 <bijinraveendran237@gmail.com>
2023-08-09 21:23:02 +0200

    Added portfolio in landingpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import LandingPageHeader from "./Components/LandingPageHeader";
+import LandingPageBody from "./Components/LandingPageBody"
+
 export default function App() {
-return (
-<>
-<div>Hello World</div>
-</>
-);
+    return (
+        <>
+            <div className="min-h-screen bg-gradient-to-tr from-[#0b61d8] via-[#120B2E] to-[#091498] 
+            px-8 lg:px-20 sm:px-10 pb-10 pt-5">
+                <LandingPageHeader/>
+                <LandingPageBody/>
+            </div>
+        </>
+    );
 }

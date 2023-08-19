84f0559dd4637df91cb4cef1698f9575aa13c60e
bijinraveendran237 <bijinraveendran237@gmail.com>
2023-08-19 13:30:31 +0200

    sample api inegration done and created a contact form

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React from "react";
 
-function Contact() {
+export default function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div className="bg-[#111560] shadow-md rounded ">
+      <section className="text-gray-700 body-font relative">
+        <div className="container px-5 py-20 mx-auto">
+          <div className="flex flex-col text-center w-full mb-10">
+            <h1 className="sm:text-3xl text-[24px] font-medium title-font mb-4 text-red-500">
+              Contact Us
+            </h1>
+          </div>
+          <div className="lg:w-1/2 md:w-2/3 mx-auto">
+            <div className="flex flex-wrap -m-2">
+              <div className="p-2 w-1/2">
+                <div className="relative">
+                  <label for="name" className="leading-7 text-[18px]  text-red-500">
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  />
+                </div>
+              </div>
+              <div className="p-2 w-1/2">
+                <div className="relative">
+                  <label
+                    for="email"
+                    className="leading-7 text-[18px]  text-red-500"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  />
+                </div>
+              </div>
+              <div className="p-2 w-full">
+                <div className="relative">
+                  <label
+                    for="message"
+                    className="leading-7 text-[18px]  text-red-500"
+                  >
+                    Message
+                  </label>
+                  <textarea
+                    id="message"
+                    name="message"
+                    className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                  ></textarea>
+                </div>
+              </div>
+              <div className="p-2 w-full">
+                <button className="flex mx-auto text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                  Button
+                </button>
+              </div>
+              <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
+                <a className="text-white">bijinraveendran237@gmail.com</a>
+                <p className="leading-normal my-5 text-red-500">
+                  Alt Grabsen
+                  <br />
+                  Hannover, Germany 30823
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
-
-export default Contact
--- src/Components/SampleAPI.jsx
+++ src/Components/SampleAPI.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import useFetchData from '../hooks/useFetchData';
+import api from '../services/api';
 
 function SampleAPI() {
-  return (
-    <div>SampleAPI</div>
-  )
+    const { data: posts, isLoading } = useFetchData(api.fetchUsers);
+
+    return (
+        <div>
+            {isLoading ? (
+                <p>Loading...</p>
+            ) : (
+                <div class="space-y-12 py-10 ">
+                    {posts.map(post => (
+                        <div class="grid border  grid-cols-5 pt-5 rounded-lg shadow-lg shadow-emerald-200 border-white border-t-50 border-l-50 hover:rounded">
+                            <div class="translate-y-4 pl-8"> </div>
+                            <h3 class="text-center text-white py-3 col-end-4 pl-2">{post.name}</h3>
+                            <h3 class="text-center text-white py-3 col-end-4 pl-2">{post.email}</h3>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
 }
+
 
 export default SampleAPI
